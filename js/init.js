354e09ecec5c0b33f1cd4d490fa988258526f1fd
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2017-11-18 23:00:04 -0500

    project selection is working

--- js/init.js
+++ js/init.js
@@ -1,12 +1,86 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
+// Materialize Design
+(function($) {
+  $(function() {
+    $(".button-collapse").sideNav();
+    $(".parallax").parallax();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+// Adding 'active' class for selected Project divs - to highlight
+// Showing projects according to selection
+window.onload = function() {
+  document.querySelector("body").addEventListener("click", function(e) {
+    if (
+      e.target.parentElement.parentElement.className ==
+      "right hide-on-med-and-down projectMenu"
+    ) {
+      if (e.target.className === "allNav") {
+        // Only selected projects
+        showProject(e.target.className);
+
+        // removing all other 'active' classes
+        removeClass(e.target.parentElement);
+      } else if (e.target.className === "nodeNav") {
+        // Only selected projects
+        showProject(e.target.className);
+
+        // removing all other 'active' classes
+        removeClass(e.target.parentElement);
+      } else if (e.target.className === "reactNav") {
+        // Only selected projects
+        showProject(e.target.className);
+
+        // removing all other 'active' classes
+        removeClass(e.target.parentElement);
+      } else if (e.target.className === "rubyNav") {
+        // Only selected projects
+        showProject(e.target.className);
+
+        // removing all other 'active' classes
+        removeClass(e.target.parentElement);
+      } else if (e.target.className === "javascriptNav") {
+        // Only selected projects
+        showProject(e.target.className);
+
+        // removing all other 'active' classes
+        removeClass(e.target.parentElement);
+      }
+    }
+  });
+};
+
+// Remove active class function
+function removeClass(current) {
+  let menuChidlren = document.querySelectorAll(".projectMenu LI");
+  // Removing every className 'active'
+  for (let i = 0; i < menuChidlren.length; i++) {
+    menuChidlren[i].classList.remove("active");
+  }
+  // Only adding 'active' to current class
+  current.classList.add("active");
+}
+
+// Projects showing by selection
+function showProject(eNav) {
+  let project = document.querySelectorAll(".projectDivs");
+  // Showing All Projects
+  project.forEach(function(e, i) {
+    // To make new selection each time.
+    e.style.display = "none";
+
+    if (eNav == "allNav") {
+      if (e.className.includes("all")) e.style.display = "block";
+    } else if (eNav == "nodeNav") {
+      if (e.className.includes("node")) e.style.display = "block";
+    } else if (eNav == "reactNav") {
+      if (e.className.includes("react")) e.style.display = "block";
+    } else if (eNav == "rubyNav") {
+      if (e.className.includes("ruby")) e.style.display = "block";
+    } else if (eNav == "javascriptNav") {
+      if (e.className.includes("javascript")) e.style.display = "block";
+    }
+  }, this);
+}
 
 /*
 	click to clear and change hue
@@ -49,7 +123,7 @@
 // 	this.life = 1;
 // 	this.decay = 0.0015;
 // 	this.dead = false;
-	
+
 // 	this.points.push({
 // 		x: this.x,
 // 		y: this.y
@@ -59,9 +133,9 @@
 // Branch.prototype.step = function( i ) {
 // 	this.life -= this.decay;
 // 	if( this.life <= 0 ) {
-// 		this.dead = true;	
+// 		this.dead = true;
 // 	}
-	
+
 // 	if( !this.dead ) {
 // 		var lastPoint = this.points[ this.points.length - 1 ];
 // 		this.points.push({
@@ -82,7 +156,7 @@
 // 	if( !this.points.length || this.dead ) {
 // 		return false;
 // 	}
-	
+
 // 	var length = this.points.length,
 // 		i = length - 1,
 // 		point = this.points[ i ],
@@ -118,9 +192,8 @@
 // 	c.width = w;
 // 	c.height = h;
 // 	tick = 0;
-	
-	
-// 	for( var i = 0; i < 500; i++ ) {		
+
+// 	for( var i = 0; i < 500; i++ ) {
 // 		branches.push( new Branch( startHue, cx, cy) );
 // 	}
 // }
@@ -144,7 +217,7 @@
 // 		ctx.drawImage( c, rand( -150, 150 ), rand( -150, 150 ) );
 // 		ctx.restore();
 // 	}
-	
+
 // 	ctx.globalCompositeOperation = 'lighter';
 // 		while( i-- ) { branches[ i ].draw() }
 // }
